71ea67c59e2f022aa00794a2d95acc1485b34454
Ezequiel Pedulla <ivanpedulla@gmail.com>
2024-03-05 13:52:34 -0300

    se agregaron las ventanas

--- main-window.py
+++ main-window.py
@@ -1,11 +1,28 @@
 
 import tkinter as tk
+from tkinter import ttk
+from tkinter import Menu
 
 
 class MainWindow:
     def __init__(self, root):
         self.root = root
         self.root.title('Stock App')
+        self.root.geometry("1000x600")
+        self.root.configure(bg='#333')
+        self.frame = tk.Frame(root, bg='#333')
+        self.frame.pack(expand=True, fill='both')
+
+        self.menu_superior = Menu(self.root)
+
+        self.menu_superior.add_command(label="Productos")
+        self.menu_superior.add_command(label="Ventas")
+        self.menu_superior.add_command(label="Clientes")
+
+        self.root.config(
+            menu=self.menu_superior,
+
+        )
 
 
 root = tk.Tk()
